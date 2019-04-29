3bee716923ce29fdb6282a2be62ec38904712fb3
Rohit <rohit@novvuminc.com>
2019-04-29 11:53:36 -0700

    dev environment

--- packages/server/src/context.ts
+++ packages/server/src/context.ts
@@ -1,5 +1,7 @@
-import { Prisma } from "./generated/prisma-client";
+import { prisma, Prisma } from "./generated/prisma-client";
 
 export type Context = {
     prisma: Prisma
 }
+
+export const getContext = { prisma } as Context
